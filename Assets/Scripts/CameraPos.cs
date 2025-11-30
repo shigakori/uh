3a74eff8abf90456c42511d84b6e31c717532e11
ВашÐkori <asdasd1@gmail.com>
2025-11-30 23:11:47 +0500

    moving n manual collision detection

---
+++ Assets/Scripts/CameraPos.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class CameraPos : MonoBehaviour
+{
+    public Transform lookAt;
+    public float boundX = 0.15f;
+    public float boundY = 0.05f;
+    void Start()
+    {
+        
+    }
+
+    void LateUpdate()
+    {
+        Vector3 delta = Vector3.zero;
+        
+        // this is to check if we're inside the bounds on the X axis
+        float deltaX = lookAt.position.x - transform.position.x;
+        if (deltaX > boundX || deltaX < -boundX)
+        {
+            if (transform.position.x < lookAt.position.x)
+            {
+                delta.x = deltaX - boundX;
+            }
+            else
+            {
+                delta.x = deltaX + boundX;
+            }
+        }
+        
+        // this is to check if we're inside the bounds on the Y axis
+        float deltaY = lookAt.position.y - transform.position.y;
+        if (deltaY > boundY || deltaY < -boundY)
+        {
+            if (transform.position.y < lookAt.position.y)
+            {
+                delta.y = deltaY - boundY;
+            }
+            else
+            {
+                delta.y = deltaY + boundY;
+            } 
+        }
+
+        transform.position += new Vector3(delta.x, delta.y, 0);
+    }
+}
