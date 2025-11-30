6febfca149ff667c7d3e91d2dfb16ddee4513a00
ВашÐkori <asdasd1@gmail.com>
2025-11-30 19:26:41 +0500

    firstscript

---
+++ Assets/FirstScript.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class FirstScript : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Hello World!");
+        Debug.Log("Hello World!!!!!!!!!!");
+    }
+}
