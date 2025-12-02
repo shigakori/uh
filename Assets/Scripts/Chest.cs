8e7d8af4b2371d9f5cc197f3564ff23ecb2c832e
aPPClanK <koksuvii@gmail.com>
2025-12-02 23:38:00 +0500

    interactive objects and inheritance

---
+++ Assets/Scripts/Chest.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Chest : Collectible
+{
+    public Sprite emptyChest;
+    public int pesosAmount = 5;
+    protected override void OnCollect()
+    {
+        if (!collected)
+        {
+            base.OnCollect();
+            GetComponent<SpriteRenderer>().sprite = emptyChest;
+            Debug.Log("Grant " + pesosAmount + " pesos!");
+        }
+        // base.OnCollect();
+        // Debug.Log("Grant pesos");
+    }
+}
---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : Collidable
+{
+    public string[] sceneNames;
+    protected override void OnCollide(Collider2D coll)
+    {
+        if(coll.name == "Player")
+        {
+            // Teleport the player
+            Debug.Log("TeleporT!");
+            string sceneName = sceneNames[Random.Range(0,sceneNames.Length)];
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
